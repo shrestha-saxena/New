40d623a4ef6f972df102b5e317e90a10f4d0f5cb
Aniket <Aniketshiv9369@gmail.com>
2023-07-14 18:43:29 +0530

    Add <filename>

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <main>
+        <h1>TechyStar</h1>
+        <p>Solution to all your probs</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home
